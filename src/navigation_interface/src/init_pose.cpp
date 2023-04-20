5abfd3e4744e9e3f7519417fbdd323d96607ddb7
Alexy <alexyskoutnev@gmail.com>
2023-04-20 20:24:25 +0000

    start

---
+++ src/navigation_interface/src/init_pose.cpp
@@ -0,0 +1,66 @@
+// Include statements 
+#include "ros/ros.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/PoseWithCovarianceStamped.h"
+#include <tf/transform_broadcaster.h>
+// #include <gazebo_ros/gazebo_ros_api_plugin.h>
+#include <iostream>
+ 
+using namespace std;
+ 
+// Initialize ROS publishers
+ros::Publisher pub;
+
+ 
+// // Take initialpose as input and publish initial_2d
+// void handle_initial_pose(const geometry_msgs::PoseWithCovarianceStamped &pose) {
+//   geometry_msgs::PoseStamped rpyPose;
+//   rpyPose.header.frame_id = "map";
+//   rpyPose.header.stamp = pose.header.stamp;
+//   rpyPose.pose.position.x = pose.pose.pose.position.x;
+//   rpyPose.pose.position.y = pose.pose.pose.position.y;
+//   rpyPose.pose.position.z = 0;
+//   tf::Quaternion q(0, 0, pose.pose.pose.orientation.z, pose.pose.pose.orientation.w);
+//   tf::Matrix3x3 m(q);
+//   double roll, pitch, yaw;
+//   m.getRPY(roll, pitch, yaw);
+//   rpyPose.pose.orientation.x = 0;
+//   rpyPose.pose.orientation.y = 0;
+//   rpyPose.pose.orientation.z = yaw;
+//   rpyPose.pose.orientation.w = 0;
+//   pub2.publish(rpyPose);
+// }
+ 
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "init_pose");
+  ros::NodeHandle node;
+  ros::NodeHandle node_service;
+//  ros::ServiceClient client = n.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state");
+  gazebo_msgs::GetModelState objstate;
+  geometry_msgs::Pose pose; //init pose of robot
+  ros::Publisher pub = node.advertise<geometry_msgs::PoseStamped>("initialpose", 1);
+  ros::Rate loop_rate(10);
+  while (ros::ok()) {
+    geometry_msgs::PoseStamped rpyPose;
+    objstate.request.model_name = "turtlebot3";
+    objstate.request.relative_entity_name = "world"; 
+    if ((client.call(objstate) && objstate.response.success)){
+      pose = objstate.request.pose;
+      std::cout << "Got the initial pose!" << std::endl;
+    }
+    rpyPose.header.frame_id = "map";
+    // rpyPose.header.stamp = 0;
+    rpyPose.pose.position.x = -2.8;
+    rpyPose.pose.position.y = 1;
+    rpyPose.pose.position.z = 0;
+    rpyPose.pose.orientation.x = 0;
+    rpyPose.pose.orientation.y = 0;
+    rpyPose.pose.orientation.z = 0;
+    rpyPose.pose.orientation.w = 1;
+    pub.publish(rpyPose);
+    std::cout << "Successfully sent pose" << std::endl;
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
---
+++ src/navigation_interface/src/navigation_interface.cpp
@@ -0,0 +1,99 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "navigation_interface");
+
+  //Init Pose Subscriber
+  
+
+  MoveBaseClient ac("move_base", true);
+  while(!ac.waitForServer(ros::Duration(5.0))){
+    ROS_INFO("Waiting for the move_base action server to come up");
+  }
+  bool run = true;
+  bool valid_flag = true;
+  int usr_choice;
+  move_base_msgs::MoveBaseGoal goal;
+  while(run){
+    goal.target_pose.header.frame_id = "map";
+    goal.target_pose.header.stamp = ros::Time::now();
+    std::cout << "Where should the robot go?" << std::endl;
+    std::cout << "1 = room 1 \n2 = room 2\n3 = room 3 \n4 = room 4 \n5 = room 5 \n6 = room 6 \n " << std::endl;
+    std::cin >> usr_choice;
+    switch(usr_choice){
+        case 1:
+            std::cout << "Going to room 1!" << std::endl;
+            goal.target_pose.pose.position.x = 5.5;
+            goal.target_pose.pose.position.y = 3.0;
+            goal.target_pose.pose.orientation.w = 1.0;
+            valid_flag = true;
+            break;
+        case 2:
+            std::cout << "Going to room 2!" << std::endl;
+            goal.target_pose.pose.position.x = 5.0;
+            goal.target_pose.pose.position.y = -3.0;
+            goal.target_pose.pose.orientation.w = 1.0;
+            valid_flag = true;
+            break;
+        case 3:
+            std::cout << "Going to room 3!" << std::endl;
+            goal.target_pose.pose.position.x = 1.0;
+            goal.target_pose.pose.position.y = 3.0;
+            goal.target_pose.pose.orientation.w = 1.0;
+            valid_flag = true;
+            break;
+        case 4:
+            std::cout << "Going to room 4!" << std::endl;
+            goal.target_pose.pose.position.x = -3.0;
+            goal.target_pose.pose.position.y = 2.5;
+            goal.target_pose.pose.orientation.w = 1.0;
+            valid_flag = true;
+            break;
+        case 5:
+            std::cout << "Going to room 5!" << std::endl;
+            goal.target_pose.pose.position.x = -6.5;
+            goal.target_pose.pose.position.y = 3.0;
+            goal.target_pose.pose.orientation.w = 1.0;
+            valid_flag = true;
+            break;
+        case 6:
+            std::cout << "Going to room 6!" << std::endl;
+            goal.target_pose.pose.position.x = -6.5;
+            goal.target_pose.pose.position.y = -2.0;
+            goal.target_pose.pose.orientation.w = 1.0;
+            valid_flag = true;
+            break;
+        default:
+            std::cout << "enter a valid number btw 1-6" << std::endl;
+            valid_flag = false;
+            ROS_INFO_STREAM("valid flag: " << valid_flag);
+        }
+
+        ROS_INFO("Sending goal !!!");
+        ac.sendGoal(goal);
+        ac.waitForResult();
+        if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
+            ROS_INFO("We reached to the goal! Time to go back");
+            goal.target_pose.pose.position.x = -2.8;
+            goal.target_pose.pose.position.y = 1.0;
+            goal.target_pose.pose.orientation.w = 1.0;
+            ac.sendGoal(goal);
+            ac.waitForResult();
+            std::cout << "Do you want to move to another spot? (Y) or (N)" << std::endl;
+            char c = getchar();
+            if (c == 'Y'){
+                continue;
+            } else {
+                run = false;
+                std::cout << "Exiting" << std::endl;
+            }
+        }
+        else
+            ROS_INFO("The base failed to move");
+    }
+  return 0;
+}
